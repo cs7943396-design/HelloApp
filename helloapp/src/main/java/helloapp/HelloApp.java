a2a05910fa9d0818ebf5aca96ecbadbb14300fb0
cs7943396-design <cs7943396@gmail.com>
2026-03-26 08:26:26 +0530

    UC2:Display Hello with Command-Line Argument

--- helloapp/src/main/java/helloapp/HelloApp.java
+++ helloapp/src/main/java/helloapp/HelloApp.java
@@ -1,13 +1,9 @@
-package helloapp;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
     }
 }
